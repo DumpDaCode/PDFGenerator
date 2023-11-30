e118354b2934395f926a9d4d005921bfc9b21658
Rajivranjan Singh <rajivranjans@mkcl.org>
2023-11-30 18:17:38 +0530

    will continue at home.

--- client/src/App.vue
+++ client/src/App.vue
@@ -4,6 +4,7 @@
 
 <script>
 import grapejs from 'grapesjs'
+// import plugin from 'grapesjs-preset-webpage';
 import 'grapesjs/dist/css/grapes.min.css'
 import 'grapesjs/dist/grapes.min.js'
 
@@ -11,42 +12,46 @@ export default {
   name: 'App',
   components: {},
   mounted() {
-    grapejs.init({
+    let editor = grapejs.init({
       container: '#gjs',
       fromElement: true,
-      height: '900px',
+      height: '95vh',
       width: 'auto',
-      blockManager: {
-        appendTo: '.myblocks',
-        blocks: [
-          {
-            id: 'image',
-            label: 'Image',
-            media: `<svg style="width:24px;height:24px" viewBox="0 0 24 24">
-            <path d="M8.5,13.5L11,16.5L14.5,12L19,18H5M21,19V5C21,3.89 20.1,3 19,3H5A2,2 0 0,0 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19Z" />
-        </svg>`,
-            // Use `image` component
-            content: { type: 'image' },
-            // The component `image` is activatable (shows the Asset Manager).
-            // We want to activate it once dropped in the canvas.
-            activate: true,
-            // select: true, // Default with `activate: true`
-          }
-        ],
+    })
+    editor.Blocks.add('my-block-id', {
+      label: 'My Block',
+      content: {
+        type: 'my-block-id',
+        content: 'My Block',
       },
-      storageManager: false
+    })
+    editor.Components.addType('my-block-id', {
+      isComponent: el => el.tagName === 'MY-BLOCK-ID',
+      model: {
+        defaults: {
+          traits: [
+            {
+              type: 'checkbox',
+              name: 'repeat',
+              label: 'Repeat',
+            },
+            {
+              type: 'select',
+              name: 'key',
+              label: 'Key',
+              options: [
+                { id: '1', name: '1' },
+                { id: '2', name: '2' },
+                { id: '3', name: '3' },
+              ],
+            }
+          ],
+        },
+      },
     })
   },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
